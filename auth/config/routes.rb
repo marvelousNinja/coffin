c590c828c614db9fc922ec481a693d09282a508b
Alexander Sologub <alexsologoub@gmail.com>
2014-01-06 20:49:15 +0300

    Auth: Bits of RailsAdmin fixes

--- auth/config/routes.rb
+++ auth/config/routes.rb
@@ -1,5 +1,5 @@
 Auth::Application.routes.draw do
-  mount RailsAdmin::Engine => '/', :as => 'rails_admin'
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
