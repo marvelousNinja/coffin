c71d38cefb44f31190431e163f87b5786fd1caaa
Alexander Sologub <alexsologoub@gmail.com>
2014-01-07 20:12:24 +0300

    Auth-lib: Required gems in engine file

--- auth_lib/lib/auth_lib/engine.rb
+++ auth_lib/lib/auth_lib/engine.rb
@@ -1,3 +1,9 @@
+require 'rubygems'
+require 'devise'
+require 'cancan'
+require 'omniauth'
+require 'omniauth-oauth2'
+
 module AuthLib
   class Engine < ::Rails::Engine
     isolate_namespace AuthLib
