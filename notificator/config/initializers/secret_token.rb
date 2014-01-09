155b7976dee47fdf1cb24a39aa48db2eb7fcf64f
Alexander Sologub <alexsologoub@gmail.com>
2014-01-09 18:01:43 +0300

    Notificator: Initial commit

---
+++ notificator/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Notificator::Application.config.secret_key_base = '75a36f4b90ec5573926405beb759fce8017e59fe9c7a5b2f5a91788b2192bf9d3720a4048b7aece21cb5c737bc75c39e6beddc32464980d91ac899d374683b9d'
