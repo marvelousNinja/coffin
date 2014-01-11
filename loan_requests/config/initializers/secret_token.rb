68a8ba41329618ba0d9bd451672dfe69747c1ea0
Alexander Sologub <alexsologoub@gmail.com>
2014-01-12 01:33:40 +0300

    Loan Requests: Initial commit

---
+++ loan_requests/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LoanRequests::Application.config.secret_key_base = '205967020dfeee4a3d1ccea4c36eb136cd0333cfb00d15c41091199e4d5fded1689dbf2815c3e26ea93e70dd36529e289f0416d5bd30f82388d07aaa9a694cdd'
---
+++ loan_requests/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LoanRequests::Application.config.session_store :cookie_store, key: '_loan_requests_session'
