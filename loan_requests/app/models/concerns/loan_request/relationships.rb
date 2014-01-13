328ce2bbd9c37333fdcc221d1b0d49a496672b9b
Alexander Sologub <alexsologoub@gmail.com>
2014-01-13 05:47:04 +0300

    Loan Requests: Updated relationships for loan requests (inverse_of)

--- loan_requests/app/models/concerns/loan_request/relationships.rb
+++ loan_requests/app/models/concerns/loan_request/relationships.rb
@@ -4,11 +4,11 @@ module Concerns::LoanRequest::Relationships
   included do
     has_paper_trail
 
-    has_one :profile
-    has_one :document
+    has_one :profile, :inverse_of => :loan_request
+    has_one :document, :inverse_of => :loan_request
     has_one :registration_address, class_name: 'Address', conditions: { address_type: :registration }
     has_one :current_address, class_name: 'Address', conditions: { address_type: :current }
-    has_one :job
+    has_one :job, :inverse_of => :loan_request
 
     accepts_nested_attributes_for :profile, :document, :registration_address, :current_address, :job
   end
