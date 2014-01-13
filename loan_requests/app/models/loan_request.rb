dc7ad83a4e3d4bb26350b9a4e9746e02a4c0f260
Alexander Sologub <alexsologoub@gmail.com>
2014-01-13 05:55:02 +0300

    Loan Requests: Forgot one of the concerns

--- loan_requests/app/models/loan_request.rb
+++ loan_requests/app/models/loan_request.rb
@@ -1,6 +1,7 @@
 class LoanRequest < ActiveRecord::Base
   include Concerns::LoanRequest::HasStateMachine
   include Concerns::LoanRequest::Relationships
+  include Concerns::LoanRequest::Enumerations
   include Concerns::LoanRequest::Validations
   include Concerns::LoanRequest::RailsAdmin
 end
