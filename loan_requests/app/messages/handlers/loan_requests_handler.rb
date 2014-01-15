7d665a2e639aeb36a2894a31eb8d6f78f61586e4
Alexander Sologub <alexsologoub@gmail.com>
2014-01-15 19:30:18 +0300

    Loan Requests: Better senders and handlers

--- loan_requests/app/messages/handlers/loan_requests_handler.rb
+++ loan_requests/app/messages/handlers/loan_requests_handler.rb
@@ -2,18 +2,19 @@ class LoanRequestsHandler
   include MqConnector::Handler
 
   def self.handle
-    channel  = create_channel auto_recovery: true
-    exchange = channel.topic('events', durable: true)
+    connect do |connection, channel|
+      exchange = channel.topic('events', durable: true)
 
-    queue = channel.queue('loan_requests', durable: true).bind(exchange, routing_key: 'loan_request.created')
+      queue = channel.queue('loan_requests', durable: true).bind(exchange, routing_key: 'loan_request.created')
 
-    queue.subscribe do |metadata, payload|
-      message = JSON.load(payload)
+      queue.subscribe do |metadata, payload|
+        loan_request_data = JSON.load(payload)
 
-      loan_request = LoanRequest.find(message['id'])
-      ApprovalBot.new.process loan_request
+        loan_request = LoanRequest.find(loan_request_data['id'])
+        ApprovalBot.new.process loan_request
 
-      puts "Received a message: #{payload}"
+        puts "Received a loan_request.created for LoanRequest##{loan_request_data['id']}"
+      end
     end
   end
 end
--- loan_requests/app/messages/senders/loan_requests_sender.rb
+++ loan_requests/app/messages/senders/loan_requests_sender.rb
@@ -2,35 +2,38 @@ class LoanRequestsSender
   include MqConnector::Sender
 
   def self.request_created(request)
-    channel = create_channel auto_recovery: true
-    exchange = channel.topic('events', durable: true)
+    connect do |connection, channel|
+      exchange = channel.topic('events', durable: true)
 
-    exchange.publish request.attributes.to_json, routing_key: 'loan_request.created', persistent: true
+      exchange.publish request.attributes.to_json, routing_key: 'loan_request.created', persistent: true
 
-    puts "Loan request has been created for #{request}"
+      puts "Loan request has been created for #{request}"
 
-    channel.close
+      disconnect
+    end
   end
 
   def self.request_approved(request)
-    channel = create_channel auto_recovery: true
-    exchange = channel.topic('events', durable: true)
+    connect do |connection, channel|
+      exchange = channel.topic('events', durable: true)
 
-    exchange.publish request.attributes.to_json, routing_key: 'loan_request.approved', persistent: true
+      exchange.publish request.attributes.to_json, routing_key: 'loan_request.approved', persistent: true
 
-    puts "Loan request has been approved for #{request}"
+      puts "Loan request has been approved for #{request}"
 
-    channel.close
+      disconnect
+    end
   end
 
   def self.request_rejected(request)
-    channel = create_channel auto_recovery: true
-    exchange = channel.topic('events', durable: true)
+    connect do |connection, channel|
+      exchange = channel.topic('events', durable: true)
 
-    exchange.publish request.attributes.to_json, routing_key: 'loan_request.rejected', persistent: true
+      exchange.publish request.attributes.to_json, routing_key: 'loan_request.rejected', persistent: true
 
-    puts "Loan request has been rejected for #{request}"
+      puts "Loan request has been rejected for #{request}"
 
-    channel.close
+      disconnect
+    end
   end
 end
