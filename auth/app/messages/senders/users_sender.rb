fbbdb56206ee0d5a613863b6dd6f060bb9738892
Alexander Sologub <alexsologoub@gmail.com>
2014-01-15 18:35:51 +0300

    Auth: Simplified sender for users

--- auth/app/messages/senders/users_sender.rb
+++ auth/app/messages/senders/users_sender.rb
@@ -2,29 +2,14 @@ class UsersSender
   include MqConnector::Sender
 
   def self.user_created(user)
-    channel = create_channel auto_recovery: true
-    exchange = channel.topic('events', durable: true)
+    self.connect do |connection, channel|
+      exchange = channel.topic('events', durable: true)
 
-    message = build_message(user)
-    exchange.publish message, routing_key: 'user.created', persistent: true
+      exchange.publish user.attributes.to_json, routing_key: 'user.created', persistent: true
 
-    log_message(user.id)
-    channel.close
-    connection.close
-  end
+      puts "Sent user.created for User##{user.id}"
 
-  private
-
-  def self.build_message(user)
-    {
-      login: user.email,
-      password: user.generated_password,
-      email: user.email,
-      phone: '+375299983953'
-    }.to_json
-  end
-
-  def self.log_message(info)
-    puts "Sent user.created for User##{info}"
+      self.disconnect
+    end
   end
 end
