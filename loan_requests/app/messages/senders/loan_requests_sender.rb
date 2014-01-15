efe559ad4abec34431c113b250aa98dfd6c5e5d1
Alexander Sologub <alexsologoub@gmail.com>
2014-01-15 19:51:19 +0300

    Loan Requests: Bit of prettyness

--- loan_requests/app/messages/senders/loan_requests_sender.rb
+++ loan_requests/app/messages/senders/loan_requests_sender.rb
@@ -7,7 +7,7 @@ class LoanRequestsSender
 
       exchange.publish request.attributes.to_json, routing_key: 'loan_request.created', persistent: true
 
-      puts "Loan request has been created for #{request}"
+      puts "Loan request has been created for LoanRequest##{request.id}"
 
       disconnect
     end
@@ -19,7 +19,7 @@ class LoanRequestsSender
 
       exchange.publish request.attributes.to_json, routing_key: 'loan_request.approved', persistent: true
 
-      puts "Loan request has been approved for #{request}"
+      puts "Loan request has been approved for LoanRequest##{request.id}"
 
       disconnect
     end
@@ -31,7 +31,7 @@ class LoanRequestsSender
 
       exchange.publish request.attributes.to_json, routing_key: 'loan_request.rejected', persistent: true
 
-      puts "Loan request has been rejected for #{request}"
+      puts "Loan request has been rejected for LoanRequest##{request.id}"
 
       disconnect
     end
