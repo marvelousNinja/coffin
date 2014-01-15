fc02e67e11c206f82d47c3478a5aee64d9296dea
Alexander Sologub <alexsologoub@gmail.com>
2014-01-15 18:34:33 +0300

    MqConnector: Better logs and connection restore

--- mq_connector/lib/mq_connector/mq_connector.rb
+++ mq_connector/lib/mq_connector/mq_connector.rb
@@ -3,12 +3,37 @@ module MqConnector
     extend ActiveSupport::Concern
 
     module ClassMethods
-      def connection
-        @connection ||= AMQP.connect
+      def connect(&client_code)
+        AMQP.start(
+          client_code: client_code,
+          auto_recovery: true,
+          on_tcp_connection_failure: method(:handle_tcp_failure),
+          on_tcp_connection_loss: method(:handle_tcp_connection_loss)) do |connection|
+          connection.on_tcp_connection_loss &method(:handle_tcp_connection_loss)
+          process(connection)
+        end
       end
 
-      def create_channel(*args, &block)
-        AMQP::Channel.new(@connection, *args, &block)
+      def process(connection)
+        AMQP::Channel.new(connection, auto_recovery: true) do |channel|
+          connection.settings[:client_code].call(connection, channel)
+        end
+      end
+
+      def disconnect
+        AMQP.stop { EM.stop }
+      end
+
+      def handle_tcp_connection_loss(connection, settings)
+        puts "[MqConnector] Lost connection. Retrying in 3 seconds..."
+        sleep 3
+        connection.reconnect(true, 1)
+      end
+
+      def handle_tcp_failure(settings)
+        puts "[MqConnector] Can't connect to broker. Retrying in 3 seconds..."
+        sleep 3
+        process(AMQP.connect(settings))
       end
     end
   end
