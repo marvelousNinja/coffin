8ced05ed06440f67dba7667b1283c427003bab6f
Alexander Sologub <alexsologoub@gmail.com>
2014-01-15 21:34:10 +0300

    Auth-lib: Let's handle OAuth::Error while we can

--- auth_lib/lib/auth_lib/engine.rb
+++ auth_lib/lib/auth_lib/engine.rb
@@ -8,5 +8,18 @@ require 'omniauth/strategies/centralized_auth'
 module AuthLib
   class Engine < ::Rails::Engine
     isolate_namespace AuthLib
+
+    initializer 'auth_lib.patch_application_controller', :after => :load_config_initializers do |app|
+      app.eager_load!
+      ApplicationController.class_eval do
+        # def after_sign_out_path_for(resource_or_scope)
+        #   'http://localhost:3000/users/sign_out'
+        # end
+
+        rescue_from OAuth2::Error do
+          sign_out_and_redirect current_user
+        end
+      end
+    end
   end
 end
