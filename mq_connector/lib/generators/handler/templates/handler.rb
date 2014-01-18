9eebaae3b2aeab2c91ce8440733eb49318f09425
Alexander Sologub <alexsologoub@gmail.com>
2014-01-18 19:02:54 +0300

    MqConnector: Updated templates

--- mq_connector/lib/generators/handler/templates/handler.rb
+++ mq_connector/lib/generators/handler/templates/handler.rb
@@ -1,15 +1,10 @@
 class <%= file_name.camelize %>Handler
   include MqConnector::Handler
 
-  def self.handle
-    channel  = create_channel auto_recovery: true
-    exchange = channel.topic('events', durable: true)
+  listen_to :<%= file_name.underscore %>
 
-    queue = channel.queue('<%= file_name %>', durable: true).bind(exchange, routing_key: '<%= file_name %>')
-
-    queue.subscribe do |metadata, payload|
-      puts "Received a message: #{payload}"
-    end
+  on :some_message_key do |message_contents|
+    ## Your processing code here
   end
 end
 
--- mq_connector/lib/generators/sender/templates/sender.rb
+++ mq_connector/lib/generators/sender/templates/sender.rb
@@ -1,13 +1,8 @@
 class <%= file_name.camelize %>Sender
   include MqConnector::Sender
 
-  def self.send_message(message)
-    channel = create_channel auto_recovery: true
-    exchange = channel.topic('events', durable: true)
-
-    exchange.publish message, routing_key: '<%= file_name %>', persistent: true
-
-    puts "Sent message: #{message}"
-    channel.close
+  def self.going_to_town
+    contents = { message: 'I am going to town' }
+    send :to => :mom, :key => :going_to_town, :with => contents
   end
 end
