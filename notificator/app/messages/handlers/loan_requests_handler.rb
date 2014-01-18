6525704c16244e0c1efda651c79cafd3129d8b00
Alexander Sologub <alexsologoub@gmail.com>
2014-01-18 21:26:28 +0300

    Auth & Notificator: Thin installed

--- notificator/app/messages/handlers/loan_requests_handler.rb
+++ notificator/app/messages/handlers/loan_requests_handler.rb
@@ -1,16 +1,18 @@
-class UsersHandler
+class LoanRequestsHandler
   include MqConnector::Handler
 
-  listen_to :users, :as => :notificator
+  listen_to :loan_requests
 
-  on :created do |user_data|
+  on :approved do |request|
     account_sid = 'AC9a2ebc1510d91b00dd7a22b6ba1547d3'
     auth_token = '36e5040fb7b727316f9b727803969d0e'
     client = Twilio::REST::Client.new account_sid, auth_token
     client.account.messages.create({
       :from => '+16122609669',
       :to => '+375299983953',
-      :body => "You have been registered, login: #{user_data['email']}, password: #{user_data['password']}"
+      :body => "Your loan request have been approved"
     })
   end
 end
+
+
