122c0c9aea9b011cbe5ec1305691da5b6da61008
Alexander Sologub <alexsologoub@gmail.com>
2014-01-20 05:33:25 +0300

    Loan Requests: Sending ALL. Let's hope that God will forgive me for that.

--- loan_requests/app/messages/senders/loan_requests_sender.rb
+++ loan_requests/app/messages/senders/loan_requests_sender.rb
@@ -3,15 +3,15 @@ class LoanRequestsSender
 
   class << self
     def created(request)
-      transmit :created, :to => :loan_requests, :with => request.attributes
+      transmit :created, :to => :loan_requests, :with => JSON.load(request.to_json(:include => [:profile, :job, :document])
     end
 
     def approved(request)
-      transmit :approved, :to => :loan_requests, :with => request.attributes.merge(:email => request.profile.email)
+      transmit :approved, :to => :loan_requests, :with => JSON.load(request.to_json(:include => [:profile, :job, :document])
     end
 
     def rejected(request)
-      transmit :rejected, :to => :loan_requests, :with => request.attributes
+      transmit :rejected, :to => :loan_requests, :with => JSON.load(request.to_json(:include => [:profile, :job, :document])
     end
   end
 end
