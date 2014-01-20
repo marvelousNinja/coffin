01ea82bff1e8659a73da321b64ea522b3a14d751
Catherine Petrusevich <cate.petrusevich@gmail.com>
2014-01-20 04:34:44 +0300

    Auth & Creditor: Translations.

--- creditor/app/models/payment.rb
+++ creditor/app/models/payment.rb
@@ -1,5 +1,8 @@
 class Payment < ActiveRecord::Base
   include Concerns::Payment::RailsAdmin
+  
+  belongs_to :loan_agreement, :inverse_of => :payments
+
   before_save do
     self.sum = basic_part.to_i + percent_part.to_i
   end
